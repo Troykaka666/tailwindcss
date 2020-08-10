04c3ba3108a3324315b6402b93d128df76a15b88
Troy Wu <troywu9527@gmail.com>
2020-08-09 21:27:07 -0400

    added tailwind css2

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,12 @@
 @section('content')
 
 
-<div class="bg-blue-700">
- <h1>hello world</h1>
+<div class="bg-blue-700 ">
+ <div class="container">
+  <h1>hello world</h1>
+  <button class="bg-blue-500 text-white font-bold py-2 px-4 rounded my-2 hover:bg-blue-200">Click me</button>
+
+  <button class="my-5 btn btn-blue">Button 2</button>
+ </div>
 </div>
 @endsection
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: '2rem'
+    },
   },
   variants: {},
   plugins: [],
